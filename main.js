ab3061e000ee09c113e6d675780c85f81e1bab3e
Felpsalx <140266345+Felpsalx@users.noreply.github.com>
2023-08-06 11:24:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ const msgerror = document.querySelector('.msgERROR');
 const camps = document.querySelector('.campos');
 
 const msgpositiva = document.querySelector('.msgPositiva');
+const msgiguais = document.querySelector('.msgIguais')
 const error = `O valor do campo <B>A</b> é maior que o valor do campo <b>B</b>`;
 const positivo = `Os valores dos campos são validos`;
+const iguais = `Os valores são iguais`;
+
 const botao = document.querySelector('#button');
 
 
@@ -15,52 +18,53 @@ const botao = document.querySelector('#button');
 
 Form.addEventListener('submit', (e)=>{
     e.preventDefault();
-    botao.disabled = true
+    msgiguais.style.display = 'none';
     msgerror.style.display = 'none';
     msgpositiva.style.display = 'none';
     campoA.value = '';
     campoB.value = '';
-    campoA.classList.remove('error')
-    campoB.classList.remove('error')
-    campoB.classList.remove('positivo')
-    campoA.classList.remove('positivo')
-    
+    campoA.classList.remove('error');
+    campoB.classList.remove('error');
+    campoB.classList.remove('positivo');
+    campoA.classList.remove('positivo');  
 })
 
 camps.addEventListener('keyup', ()=>{
     
     if(campoB.value > campoA.value){
-        
-        botao.disabled = false
-        botao.style = ''
+        botao.disabled = false;
+        botao.style = '';
+        msgpositiva.innerHTML = positivo;
         msgerror.style.display = 'none';
-        msgpositiva.innerHTML = positivo
-        msgpositiva.style.display= 'block'
-        campoA.classList.remove('error')
-        campoB.classList.add('positivo')
-        campoA.classList.add('positivo')
+        msgiguais.style.display = 'none';
+        msgpositiva.style.display= 'block';
+        campoA.classList.remove('error');
+        campoB.classList.add('positivo');
+        campoA.classList.add('positivo');
 
     }else if(campoB.value === campoA.value){
-        botao.disabled = false
-        botao.style = ''
-        
+        botao.disabled = false;
+        botao.style = '';
+        msgiguais.style.display = 'block';
+        msgiguais.innerHTML = iguais;
         msgpositiva.style.display = 'none';
         msgerror.style.display = 'none';
-        campoA.classList.remove('error')
-        campoB.classList.remove('positivo')
+        campoA.classList.remove('error');
+        campoB.classList.remove('positivo');
         campoB.classList.remove('error');
-        campoA.classList.remove('positivo')
+        campoA.classList.remove('positivo');
+
     }else{
-        botao.disabled = true
+        botao.disabled = true;
         msgerror.innerHTML = error;
         botao.style.cursor = 'not-allowed'
         msgpositiva.style.display = 'none';
-        msgerror.style.display = 'block'
+        msgiguais.style.display = 'none';
+        msgerror.style.display = 'block';
         campoA.classList.add('error');
         campoB.classList.add('error');
         campoB.classList.remove('positivo');
         campoA.classList.remove('positivo');
-        
     }
 })
 
