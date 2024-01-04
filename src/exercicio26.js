1e6ab6fb067979ebfd2b9ad10d51b016a174aa68
Felipe alexandre <lipe.alx@gmail.com>
2024-01-04 16:48:09 -0300

    first commit

---
+++ src/exercicio26.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplicar(5, 22));
+function saudacoes(nome) {
+    return "olá " + nome;
+}
+console.log(saudacoes("felipe"));
---
+++ src/exercicio26.ts
@@ -0,0 +1,13 @@
+function multiplicar(num1:number, num2:number): number{
+    return num1 * num2
+}
+
+console.log(multiplicar(5,22))
+
+function saudacoes(nome:string):string{
+    return "olá " + nome
+}
+
+console.log(saudacoes("felipe"))
+
+
